9c403c5e56d7988b4594aa874998be5bdb179f33
TarunThakur7974 <tarunsinghthakur6261@gmail.com>
2023-12-10 11:40:31 +0530

    ek

--- db_config/db_config.js
+++ db_config/db_config.js
@@ -3,11 +3,7 @@ require('dotenv').config()
 
 const connectToDatabase = async () => {
     try {
-      const connect = await mongoose.connect(process.env.URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    poolSize: 10,
-});
+      const connect = await mongoose.connect(process.env.URI);
         console.log("connect")
     } catch (error) {
         console.log(error)
