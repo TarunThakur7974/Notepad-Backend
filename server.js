e6c3ba3b264b7b9354b03e6a5542bbdfa7826cca
TarunThakur7974 <tarunsinghthakur6261@gmail.com>
2023-12-11 11:28:03 +0530

    space

--- server.js
+++ server.js
@@ -9,29 +9,13 @@ require('dotenv').config();
 connectToDatabase();
 
 const PORT = process.env.PORT || 8000
-// app.use(cors({
-//     origin: 'https://notepad-five-tau.vercel.app/' || "https://notepad-five-tau.vercel.app/**",
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     credentials: true,
-//     optionsSuccessStatus: 200,
-// }));
-// if(cors.origin){
-//     console.log(cors.origin)
-// }
+app.use(cors({
+    origin: 'https://notepad-five-tau.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 200,
+}));
 
-
-const customCorsMiddleware = (req, res, next) => {
-    const frontendDomain = req.get('Origin');
-
-    if (frontendDomain && frontendDomain.includes('https://notepad-five-tau.vercel.app')) {
-        res.header('Access-Control-Allow-Origin', frontendDomain);
-        res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-        res.header('Access-Control-Allow-Credentials', true);
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    }
-    next();
-};
-app.use(customCorsMiddleware);
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
